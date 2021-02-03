3fcc3ed0ae0a1454424576cbd00829ee87ab4ba5
alonso <alonsovillegas2016@gmail.com>
2021-02-03 11:29:18 -0500

    Add http control with ResponseEntity

--- src/main/java/com/av/biv/web/controller/UserController.java
+++ src/main/java/com/av/biv/web/controller/UserController.java
@@ -3,6 +3,8 @@ package com.av.biv.web.controller;
 import com.av.biv.domain.User;
 import com.av.biv.domain.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -15,33 +17,40 @@ public class UserController {
   private UserService userService;
 
   @GetMapping("/all")
-  public List<User> getAllUsers() {
-    return userService.getAll();
+  public ResponseEntity<List<User>> getAllUsers() {
+    return new ResponseEntity<>(userService.getAll(), HttpStatus.OK);
   };
 
   @GetMapping("/id/{id}")
-  public Optional<User> getUser(@PathVariable("id") int userId) {
-    return userService.getUser(userId);
+  public ResponseEntity<User> getUser(@PathVariable("id") int userId) {
+    return userService.getUser(userId).map(user -> new ResponseEntity<>(user, HttpStatus.OK))
+            .orElse(new ResponseEntity(HttpStatus.NOT_FOUND));
   };
 
   @GetMapping("/name/{name}")
-  public Optional<List<User>> getByName(@PathVariable String name) {
-    return userService.getByName(name);
+  public ResponseEntity<List<User>> getByName(@PathVariable String name) {
+    return userService.getByName(name).map(users -> new ResponseEntity(users, HttpStatus.OK))
+            .orElse(new ResponseEntity(HttpStatus.NOT_FOUND));
   };
 
   @GetMapping("/email/{email}")
-  public Optional<User> getByEmail(@PathVariable String email) {
-    return userService.getByEmail(email);
+  public ResponseEntity<User> getByEmail(@PathVariable String email) {
+    return userService.getByEmail(email).map(user -> new ResponseEntity(user, HttpStatus.OK))
+            .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
   };
 
   @PostMapping("/save")
-  public User save(@RequestBody User user) {
-    return userService.save(user);
+  public ResponseEntity<User> save(@RequestBody User user) {
+    return new ResponseEntity<>(userService.save(user), HttpStatus.CREATED);
   };
 
   @PostMapping("/delete/{id}")
-  public boolean delete(@PathVariable("id") int userId) {
-    return userService.delete(userId);
+  public ResponseEntity delete(@PathVariable("id") int userId) {
+    if (userService.delete(userId)) {
+      return new ResponseEntity<>(HttpStatus.OK);
+    } else {
+      return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
   };
 
 }
