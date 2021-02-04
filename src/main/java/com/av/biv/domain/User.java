b111589aa775daca77b7d8f47eb1dd4b4f61b5d1
alonso <alonsovillegas2016@gmail.com>
2021-02-04 18:36:29 -0500

    Add remaining attributes to users

--- src/main/java/com/av/biv/domain/User.java
+++ src/main/java/com/av/biv/domain/User.java
@@ -1,9 +1,15 @@
 package com.av.biv.domain;
 
+import java.sql.Date;
+
 public class User {
   private String name;
-  private int userId;
+  private int id;
   private String email;
+  private String username;
+  private String lastName;
+  private String password;
+  private Date birthDate;
 
   public String getName() {
     return name;
@@ -13,12 +19,36 @@ public class User {
     this.name = name;
   }
 
-  public int getUserId() {
-    return userId;
+  public String getLastName() {
+    return lastName;
   }
 
-  public void setUserId(int userId) {
-    this.userId = userId;
+  public void setLastName(String lastName) {
+    this.lastName = lastName;
+  }
+
+  public String getPassword() {
+    return password;
+  }
+
+  public void setPassword(String password) {
+    this.password = password;
+  }
+
+  public int getId() {
+    return id;
+  }
+
+  public void setId(int id) {
+    this.id = id;
+  }
+
+  public String getUsername() {
+    return username;
+  }
+
+  public void setUsername(String username) {
+    this.username = username;
   }
 
   public String getEmail() {
@@ -27,5 +57,13 @@ public class User {
 
   public void setEmail(String email) {
     this.email = email;
+  }
+
+  public Date getBirthDate() {
+    return birthDate;
+  }
+
+  public void setBirthDate(Date birthDate) {
+    this.birthDate = birthDate;
   }
 }
--- src/main/java/com/av/biv/persintance/entity/UserEntity.java
+++ src/main/java/com/av/biv/persintance/entity/UserEntity.java
@@ -1,6 +1,7 @@
 package com.av.biv.persintance.entity;
 
 import javax.persistence.*;
+import java.sql.Date;
 
 @Entity
 @Table(name = "users")
@@ -8,19 +9,28 @@ public class UserEntity {
 
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
-  @Column(name = "user_id")
-  private Integer userId;
+  private Integer id;
 
   private String name;
 
   private String email;
 
-  public Integer getUserId() {
-    return userId;
+  private String username;
+
+  @Column(name = "last_name")
+  private String lastName;
+
+  private String password;
+
+  @Column(name = "birthdate")
+  private Date birthDate;
+
+  public Integer getId() {
+    return id;
   }
 
-  public void setUserId(Integer userId) {
-    this.userId = userId;
+  public void setId(Integer id) {
+    this.id = id;
   }
 
   public String getName() {
@@ -31,11 +41,43 @@ public class UserEntity {
     this.name = name;
   }
 
+  public String getLastName() {
+    return lastName;
+  }
+
+  public void setLastName(String lastName) {
+    this.lastName = lastName;
+  }
+
+  public String getPassword() {
+    return password;
+  }
+
+  public void setPassword(String password) {
+    this.password = password;
+  }
+
+  public String getUsername() {
+    return username;
+  }
+
+  public void setUsername(String username) {
+    this.username = username;
+  }
+
   public String getEmail() {
     return email;
   }
 
   public void setEmail(String email) {
     this.email = email;
+  }
+
+  public Date getBirthDate() {
+    return birthDate;
+  }
+
+  public void setBirthDate(Date birthDate) {
+    this.birthDate = birthDate;
   }
 }
